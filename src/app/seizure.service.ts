38535f65872a60c1199d45c865ce8bad60bfad84
apope8 <apope8@gmail.com>
2018-07-12 12:59:45 +0100

    Set Up Seizure Services

--- src/app/seizure.service.ts
+++ src/app/seizure.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import { Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Seizure } from './seizure';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeizureService {
+  private seizuresURL = 'http://localhost:8080/seizures'; //URL to API
+  
+  constructor(
+    private http: HttpClient
+  ){}
+  
+  getCustomers (): Observable<Seizure[]> {
+    return this.http.get<Seizure[]>(this.seizuresURL)
+  }
 
-  constructor() { }
+  getSeizure(id: number): Observable<Seizure> {
+    const url = `${this.seizuresURL}/${id}`;
+    return this.http.get<Seizure>(url);
+  }
+
+  addSeizure (seizure : Seizure): Observable<Seizure> {
+    return this.http.post<Seizure>(this.seizuresURL, seizure, httpOptions);
+  }
+
+  deleteSeizure (seizure: Seizure | number): Observable<Seizure> {
+    const id = typeof seizure === 'number' ? seizure : seizure.id;
+    const url = `${this.seizuresURL}/${id}`;
+
+    return this.http.delete<Seizure>(url, httpOptions);
+  }
+
+  updateSeizure (seizure : Seizure): Observable<any> {
+    return this.http.put(this.seizuresURL, seizure, httpOptions);
+  }
+
+
+
 }
