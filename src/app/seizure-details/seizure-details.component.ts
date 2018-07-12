c304c086b19e376088b2a64d2032a77144a9c7a9
apope8 <apope8@gmail.com>
2018-07-12 16:11:28 +0100

    Start Seizure Component Structure

--- src/app/seizure-details/seizure-details.component.ts
+++ src/app/seizure-details/seizure-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Seizure } from '../seizure';
+import { SeizureService } from '../seizure.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-seizure-details',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeizureDetailsComponent implements OnInit {
 
-  constructor() { }
+  seizure = new Seizure();
+  submitted = false;
+  message: string;
 
-  ngOnInit() {
+  constructor(
+    private seizureSevice: SeizureService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.seizureSevice.getSeizure(id)
+    .subscribe(Seizure => this.seizure = Seizure);
+  }
+
+  update(): void {
+    this.submitted = true;
+    this.seizureSevice.updateSeizure(this.seizure)
+    .subscribe(() => this.message = "Seizure Record has been updated");
+  }
+
+  delete(): void{
+    this.submitted = true;
+    this.seizureSevice.deleteSeizure(this.seizure.id)
+    .subscribe(()=> this.message = "Seizure Record has been Removed")
+  }
+
+  goBack(): void {
+    this.location.back()
   }
 
 }
