c987183c354e89509133e9d7ac078c9f5f43bb8e
apope8 <apope8@gmail.com>
2018-07-16 21:50:23 +0100

    Medication Details begin

--- src/app/medication-details/medication-details.component.ts
+++ src/app/medication-details/medication-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Medication} from '../medication';
+import {MedicationService} from '../medication.service';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-medication-details',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicationDetailsComponent implements OnInit {
 
-  constructor() { }
+  medication = new Medication();
+  submitted = false;
+  message: string;
 
-  ngOnInit() {
+  constructor(
+    private medicationService: MedicationService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.medicationService.getMedication(id)
+      .subscribe(medication => this.medication = medication);
+  }
+
+  update(): void {
+    this.submitted = true;
+    this.medicationService.updateMedication(this.medication.id)
+      .subscribe(() => this.message = 'Medication record has been updated');
+  }
+
+  delete(): void {
+    this.submitted = true;
+    this.medicationService.deleteMedication(this.medication.id)
+      .subscribe(() => this.message = 'Medication record has been removed');
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
