3c8367fb989bb7b2b1f48f6079732804a0c21fa8
apope8 <apope8@gmail.com>
2018-07-16 22:28:42 +0100

    Add new Medication

--- src/app/add-medication/add-medication.component.ts
+++ src/app/add-medication/add-medication.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {MedicationService} from '../medication.service';
+import {Location} from '@angular/common';
+import {Medication} from '../medication';
 
 @Component({
   selector: 'app-add-medication',
   templateUrl: './add-medication.component.html',
   styleUrls: ['./add-medication.component.css']
 })
-export class AddMedicationComponent implements OnInit {
+export class AddMedicationComponent {
 
-  constructor() { }
+  medication = new Medication();
+  submitted = false;
 
-  ngOnInit() {
+  constructor(
+    private medicationService: MedicationService,
+    private location: Location
+  ) { }
+
+  newMedication(): void{
+    this.submitted = false;
+    this.medication = new Medication();
+  }
+
+  addMedication() {
+    this.submitted = true;
+    this.save();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  private save(): void {
+    this.medicationService.addMedication(this.medication)
+      .subscribe();
   }
 
 }
